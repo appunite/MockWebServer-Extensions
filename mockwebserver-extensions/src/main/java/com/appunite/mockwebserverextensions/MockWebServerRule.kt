4e73e70faa5daac79253b18df6fd35ca177f5960
nowakweronika <nowakweronika1301@gmail.com>
2024-03-29 15:34:13 +0100

    Add wrapDispatcher method.

--- mockwebserver-extensions/src/main/java/com/appunite/mockwebserverextensions/MockWebServerRule.kt
+++ mockwebserver-extensions/src/main/java/com/appunite/mockwebserverextensions/MockWebServerRule.kt
@@ -5,6 +5,7 @@ import com.appunite.mockwebserverextensions.intercept.UrlOverrideInterceptor
 import com.appunite.mockwebserverextensions.util.ResponseGenerator
 import okhttp3.Interceptor
 import okhttp3.internal.closeQuietly
+import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
@@ -19,8 +20,8 @@ open class MockWebServerRule(
         val LOG: Logger = Logger.getLogger(MockWebServerRule::class.java.name)
     }
 
-    open var mockDispatcher: MockDispatcher = MockDispatcher()
-    open var mockWebServer: MockWebServer? = null
+    private var mockDispatcher: MockDispatcher = MockDispatcher()
+    private var mockWebServer: MockWebServer? = null
 
     override fun register(response: ResponseGenerator) = mockDispatcher.register(response)
 
@@ -28,7 +29,7 @@ open class MockWebServerRule(
 
     override fun starting(description: Description?) {
         val server = MockWebServer()
-        server.dispatcher = mockDispatcher
+        server.dispatcher = wrapDispatcher(mockDispatcher)
         TestInterceptor.testInterceptor = interceptor ?: UrlOverrideInterceptor(server.url("/"))
         LOG.info("TestInterceptor installed")
         mockWebServer = server
@@ -43,4 +44,6 @@ open class MockWebServerRule(
 
         MultipleFailureException.assertEmpty(mockDispatcher.errors)
     }
+
+    open fun wrapDispatcher(mockDispatcher: MockDispatcher): Dispatcher = mockDispatcher
 }
